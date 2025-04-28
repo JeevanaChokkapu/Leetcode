455a15e7101772822caa5c5d7520c3c1a4b75782
JeevanaChokkapu <chokkapujeevana345@gmail.com>
2025-04-28 21:01:30 +0530

    Time: 2 ms (99.26%), Space: 48.4 MB (30.41%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-      ArrayList<Integer> l=new ArrayList<>();
-      HashMap<Integer,Integer> h=new HashMap<>();
-      h.put(nums[0],1);
-      for(int i=1;i<nums.length;i++)
-      {
-        if(!h.containsKey(nums[i]))
-             h.put(nums[i],1);
-        else
-            h.put(nums[i],h.getOrDefault(nums[i],0)+1);
-      }  
-      for(Map.Entry<Integer,Integer> x:h.entrySet())
-      {
-        if(x.getValue()>nums.length/3)
-        l.add(x.getKey());
-      }
-      return l;
+     ArrayList<Integer> ls=new ArrayList<>();
+     int el1=Integer.MIN_VALUE;
+     int el2=Integer.MAX_VALUE;
+     int cnt1=0;int cnt2=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        if(cnt1==0 && nums[i]!=el2)
+        {
+            cnt1++;
+            el1=nums[i];
+        }
+        else if(cnt2==0 && nums[i]!=el1)
+        {
+            cnt2++;
+            el2=nums[i];
+        }
+        else if(nums[i]==el1)
+        cnt1++;
+        else if(nums[i]==el2)
+        cnt2++;
+        else{
+            cnt1--;cnt2--;
+        }
+     }
+      cnt1=0; cnt2=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        if(el1==nums[i])
+        cnt1++;
+        if(el2==nums[i])
+        cnt2++;
+     }
+     if(cnt1>nums.length/3)
+     ls.add(el1);
+     if(cnt2>nums.length/3)
+     ls.add(el2);
+     return ls;
     }
 }
