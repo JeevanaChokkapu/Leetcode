d297b63dd60b4a5adcd2e266e9b7627a08dba9eb
JeevanaChokkapu <chokkapujeevana345@gmail.com>
2026-01-24 13:43:21 +0530

    Time: 3 ms (20.12%), Space: 47.8 MB (66.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length==1)
-        return ;
-        if(nums.length==2)
-        {
-            if(nums[0]==0 && nums[1]!=0)
-            {
-                int temp=nums[0];
-                nums[0]=nums[1];
-                nums[1]=temp;
-            }
-            return;
-        }
+        // if(nums.length==1)
+        // return ;
+        // if(nums.length==2)
+        // {
+        //     if(nums[0]==0 && nums[1]!=0)
+        //     {
+        //         int temp=nums[0];
+        //         nums[0]=nums[1];
+        //         nums[1]=temp;
+        //     }
+        //     return;
+        // }
         int i=0,j=1;
         while(j<nums.length)
         {
