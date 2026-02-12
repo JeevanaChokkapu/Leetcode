c3fbbf73eb77806343a0453899a05c0ff8f8058c
JeevanaChokkapu <chokkapujeevana345@gmail.com>
2026-02-12 21:15:14 +0530

    Time: 9 ms (5.53%), Space: 47.2 MB (5.41%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> res=new ArrayList<>();
+        generateAll("",n,res);
+       return res;
+        
+    }
+    public boolean isValid(String curr,int n, List<String> res)
+{
+    int balance=0;
+    for(int i=0;i<2*n;i++)
+    {
+        if(curr.charAt(i)=='(')
+        balance++;
+        else
+        balance--;
+        if(balance<0)
+        return false;
+    }
+    return balance==0;
+}
+public void generateAll(String curr,int n,List<String> res)
+{
+    if(curr.length()==2*n)
+     {
+        if(isValid(curr,n,res))
+        res.add(curr);
+        return;
+     }
+     generateAll(curr+'(',n,res);
+     generateAll(curr+')',n,res);
+}
+
+
+}
